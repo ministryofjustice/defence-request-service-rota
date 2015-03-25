cabdad5e16b32f392b8ab9d513b557618fbd5404
Pedro Moreira <pedro.moreira@unboxedconsulting.com>
2015-03-25 14:26:44 +0000

    Adds Service Registry initializer
    This enables a dictionary of services to be maintained, allowing easy
    swap across environments.

---
+++ config/initializers/service_registry.rb
@@ -0,0 +1,23 @@
+module DefenceRequestServiceRota
+  def self.services(name, service = nil)
+    @services ||= {}
+
+    if service
+      @services[name] = service
+      return true
+    else
+      if @services[name]
+        return @services[name]
+      else
+        raise ServiceNotRegisteredException.new(name)
+      end
+    end
+  end
+
+  class ServiceNotRegisteredException < Exception; end
+end
+
+## Example usage:
+## 
+## require service file
+## DefenceRequestServiceRota.services(:service, service.new(args))
