a45c303a22abab71507beae3f33a36120d64aaed
Chris Carter <chris.carter@unboxedconsulting.com>
2015-03-30 10:55:57 +0100

    Pull defence_request Omniauth strategy from gem instead of locally

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-require 'omniauth'
-require File.join(Rails.root, 'lib/omniauth/strategies/defence_request')
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :defence_request, Settings.authentication.application_id, Settings.authentication.application_secret
 end
