b87428f1f95223289f2da0b398ebff33a949e5b7
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-08 11:25:09 +0100

    Update test to use new sign in helpers

--- spec/features/user_views_organisations_list_spec.rb
+++ spec/features/user_views_organisations_list_spec.rb
@@ -1,8 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "User views a list of organisations" do
+  def sign_in_using_dsds_auth
+    visit root_path
+  end
+
   scenario "on their dashboard" do
     expected_organisation_names = "Tuckers", "Brighton"
+
+    mock_token
+    mock_profile
 
     sign_in_using_dsds_auth
 
