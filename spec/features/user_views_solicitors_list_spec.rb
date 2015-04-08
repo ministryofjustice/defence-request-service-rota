a7cf960b061f0de426a6186c5927c269c8735c24
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-08 11:21:05 +0100

    Update test to use new sign in helpers

--- spec/features/user_views_solicitors_list_spec.rb
+++ spec/features/user_views_solicitors_list_spec.rb
@@ -1,8 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "User views a list of solicitors" do
+  def sign_in_using_dsds_auth
+    visit root_path
+  end
+
   scenario "on their dashboard" do
     expected_solicitor_names = "Bob Smith", "Andy Brown"
+
+    mock_token
+    mock_profile
 
     sign_in_using_dsds_auth
 
