e89cc83c2bccf6bfab7e32d1062324a1541da6c8
Chris Carter <chris.carter@unboxedconsulting.com>
2015-05-15 14:27:40 +0100

    Edit library classes to reflect new date structure

--- lib/rota_generation/allocator.rb
+++ lib/rota_generation/allocator.rb
@@ -6,8 +6,8 @@ module RotaGeneration
 
     def mutate_slots!(solution_clauses)
       solution_clauses.each do |clause|
-        shift_id, date_of_month, firm_uid = split_clause(clause)
-        date = Date.new(2015, 5, date_of_month.to_i)
+        shift_id, date_of_month, month, year, firm_uid = split_clause(clause)
+        date = Date.new(year.to_i, month.to_i, date_of_month.to_i)
         matching_slot = slots.detect { |s| s.date == date && s.shift_id == shift_id.to_i }
         matching_slot.organisation_uid = firm_uid
         matching_slot.save!
@@ -17,7 +17,7 @@ module RotaGeneration
     private
 
     def split_clause(clause)
-      clause.match(/allocated\(([^,]*),[^,]*,(\d+),\"([^,]*)\"\)/).captures
+      clause.match(/allocated\(([^,]*),[^,]*,(\d+),(\d+),(\d+),\"([^,]*)\"\)/).captures
     end
 
     attr_reader :slots, :solution
