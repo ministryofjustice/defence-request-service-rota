3586033771b29af735519831b1fc97aac7a9f455
Chris Carter <chris.carter@unboxedconsulting.com>
2015-05-18 13:32:30 +0100

    Add tests for three of the five writer classes

---
+++ spec/lib/rota_generation/organisation_writer_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require_relative "../../../lib/rota_generation/organisation_writer"
+
+RSpec.describe RotaGeneration::OrganisationWriter do
+  describe "#write!" do
+    it "writes the organisations out to the provided file" do
+      organisations = [
+        double(:organisation, uid: "12345"),
+        double(:organisation, uid: "98765")
+      ]
+
+      fake_file = StringIO.new
+
+      RotaGeneration::OrganisationWriter.new(organisations, fake_file).write!
+
+      expect(fake_file.string).to eq <<-FILE
+firm("12345").
+firm("98765").
+      FILE
+    end
+  end
+end
