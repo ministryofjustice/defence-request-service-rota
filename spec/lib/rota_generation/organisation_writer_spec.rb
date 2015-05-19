413e130026fd21caa2deebbe7e77448cf89f9d1e
Chris Carter <chris.carter@unboxedconsulting.com>
2015-05-19 09:13:17 +0100

    Fix broken spec

--- spec/lib/rota_generation/organisation_writer_spec.rb
+++ spec/lib/rota_generation/organisation_writer_spec.rb
@@ -4,7 +4,7 @@ require_relative "../../../lib/rota_generation/organisation_writer"
 RSpec.describe RotaGeneration::OrganisationWriter do
   describe "#write!" do
     it "writes the organisations out to the provided file" do
-      organisation_uids = %w{"12345", "98765"}
+      organisation_uids = %w{12345 98765}
 
       fake_file = StringIO.new
 
