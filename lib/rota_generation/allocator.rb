bf8970903240970f983bedf93198726b1fea9af6
Chris Carter <chris.carter@unboxedconsulting.com>
2015-05-19 10:18:10 +0100

    Re-add Allocator class and test

---
+++ lib/rota_generation/allocator.rb
@@ -0,0 +1,20 @@
+module RotaGeneration
+  class Allocator
+    def mutate_slots!(slots, solution_clauses)
+      solution_clauses.each do |clause|
+        shift_id, date_of_month, month, year, firm_uid = split_clause(clause)
+        date = Date.new(year.to_i, month.to_i, date_of_month.to_i)
+        matching_slot = slots.detect { |s| s.date == date && s.shift_id == shift_id.to_i }
+        matching_slot.organisation_uid = firm_uid if matching_slot
+      end
+
+      slots
+    end
+
+    private
+
+    def split_clause(clause)
+      clause.match(/allocated\(([^,]*),[^,]*,(\d+),(\d+),(\d+),\"([^,]*)\"\)/).captures
+    end
+  end
+end
--- lib/rota_generation/parser.rb
+++ lib/rota_generation/parser.rb
@@ -18,23 +18,8 @@ module RotaGeneration
       Solution.new(optimal_solution)
     end
 
-    def mutate_slots!(slots, solution_clauses)
-      solution_clauses.each do |clause|
-        shift_id, date_of_month, month, year, firm_uid = split_clause(clause)
-        date = Date.new(year.to_i, month.to_i, date_of_month.to_i)
-        matching_slot = slots.detect { |s| s.date == date && s.shift_id == shift_id.to_i }
-        matching_slot.organisation_uid = firm_uid
-      end
-
-      slots
-    end
-
     private
 
     attr_reader :clingo_response
-
-    def split_clause(clause)
-      clause.match(/allocated\(([^,]*),[^,]*,(\d+),(\d+),(\d+),\"([^,]*)\"\)/).captures
-    end
   end
 end
