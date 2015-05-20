8d3b2d0399f446f4bc1b407308dfeea1a97ae71b
Chris Carter <chris.carter@unboxedconsulting.com>
2015-05-20 09:56:56 +0100

    Ensure that we don't assign organisation_uids to a RotaSlot more than once

--- lib/rota_generation/allocator.rb
+++ lib/rota_generation/allocator.rb
@@ -4,7 +4,7 @@ module RotaGeneration
       solution_clauses.each do |clause|
         shift_id, date_of_month, month, year, firm_uid = split_clause(clause)
         date = Date.new(year.to_i, month.to_i, date_of_month.to_i)
-        matching_slot = slots.detect { |s| s.date == date && s.shift_id == shift_id.to_i }
+        matching_slot = slots.detect { |s| s.date == date && s.shift_id == shift_id.to_i && s.organisation_uid == nil }
         matching_slot.organisation_uid = firm_uid if matching_slot
       end
 
