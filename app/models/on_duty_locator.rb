86521a467bce005627f4262a8e2e8215fbb0cc83
Chris Carter <chris.carter@unboxedconsulting.com>
2015-06-12 15:03:16 +0100

    Add extra tests and return empty if in a gap between slots

--- app/models/on_duty_locator.rb
+++ app/models/on_duty_locator.rb
@@ -19,7 +19,7 @@ class OnDutyLocator
   def slots_with_on_duty_shift
     applicable_slot = rota_slots.order(starting_time: :desc).detect { |slot| slot.starting_time <= time }
 
-    if applicable_slot.nil?
+    if applicable_slot.nil? || (applicable_slot.ending_time.present? && applicable_slot.ending_time < time)
       []
     else
       rota_slots.where(starting_time: applicable_slot.starting_time, shift_id: applicable_slot.shift_id)
--- spec/models/on_duty_locator_spec.rb
+++ spec/models/on_duty_locator_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe OnDutyLocator, "#locate" do
 
   let(:first_shift) { build_stubbed :shift, starting_time: Time.parse("12:00") }
   let(:last_shift)  { build_stubbed :shift, starting_time: Time.parse("21:00") }
+  let(:defined_shift) { build_stubbed :shift, starting_time: Time.parse("08:00"), ending_date: Time.parse("16:00") }
 
-  it "returns the uids for the organisations on duty" do
+  it "returns the single uid for the organisation on duty" do
     time = DateTime.parse("01/01/2014 20:00").iso8601
 
     create(:rota_slot, organisation_uid: firm_1, starting_time: DateTime.parse("01/01/2014 12:00"), shift: first_shift)
@@ -19,7 +20,7 @@ RSpec.describe OnDutyLocator, "#locate" do
     expect(on_duty_organisation_uids).to eq [firm_1]
   end
 
-  it "returns the uids for the organisations on duty" do
+  it "returns multiple uids for the organisations on duty" do
     time = DateTime.parse("01/01/2014 20:00").iso8601
 
     create(:rota_slot, organisation_uid: firm_1, starting_time: DateTime.parse("01/01/2014 12:00"), shift: first_shift)
@@ -31,7 +32,7 @@ RSpec.describe OnDutyLocator, "#locate" do
     expect(on_duty_organisation_uids).to match_array([firm_1, firm_2])
   end
 
-  it "returns the uids for the organisations on duty" do
+  it "returns the correct uid even if it not the first slot in time" do
     time = DateTime.parse("01/01/2014 22:00").iso8601
 
     create(:rota_slot, organisation_uid: firm_1, starting_time: DateTime.parse("01/01/2014 12:00"), shift: first_shift)
@@ -41,5 +42,36 @@ RSpec.describe OnDutyLocator, "#locate" do
     on_duty_organisation_uids = OnDutyLocator.new(time, RotaSlot.all).locate
 
     expect(on_duty_organisation_uids).to match_array([firm_3])
+  end
+
+  it "returns no organisation uids if in a defined gap between shifts" do
+    time = DateTime.parse("01/01/2014 17:00")
+
+    create(:rota_slot,
+           organisation_uid: firm_1,
+           starting_time: DateTime.parse("01/01/2014 08:00"),
+           ending_time: DateTime.parse("01/01/2014 16:00"))
+
+    create(:rota_slot,
+           organisation_uid: firm_1,
+           starting_time: DateTime.parse("02/01/2014 08:00"),
+           ending_time: DateTime.parse("02/01/2014 16:00"))
+
+    on_duty_organisation_uids = OnDutyLocator.new(time, RotaSlot.all).locate
+
+    expect(on_duty_organisation_uids).to match_array([])
+  end
+
+  it "returns no organisation uids if before all slots" do
+    time = DateTime.parse("01/01/2014 06:00")
+
+    create(:rota_slot,
+           organisation_uid: firm_1,
+           starting_time: DateTime.parse("01/01/2014 08:00"),
+           ending_time: DateTime.parse("01/01/2014 16:00"))
+
+    on_duty_organisation_uids = OnDutyLocator.new(time, RotaSlot.all).locate
+
+    expect(on_duty_organisation_uids).to match_array([])
   end
 end
