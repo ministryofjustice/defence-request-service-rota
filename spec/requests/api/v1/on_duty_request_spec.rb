3b75bf8f9db9c2ce7643bd27017ad9665b818cc6
Chris Carter <chris.carter@unboxedconsulting.com>
2015-06-19 11:55:36 +0100

    We don't require a token to call this endpoint yet

--- spec/requests/api/v1/on_duty_request_spec.rb
+++ spec/requests/api/v1/on_duty_request_spec.rb
@@ -22,8 +22,7 @@ RSpec.describe "GET /v1/on_duty_firm/:location_uid/:time" do
 
     get "/v1/on_duty_firm/", {
       location_uid: "93b8ef50-fe12-4d80-9e7e-05e98232ec13",
-      time: Time.parse("01/01/2014 20:00").iso8601,
-      token: "611151277c992292868f772d573da4eea4ade37e303582b328c674e8ce69b512",
+      time: Time.parse("01/01/2014 20:00").iso8601
     }
 
     expect(response_json).to eq({ "organisation_uids" => [on_duty_firm[:uid]] })
