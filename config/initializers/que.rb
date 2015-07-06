f53367cf13bbf18638e06f5f651fd50732290557
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-06 14:27:40 +0100

    Add little comment for que initializer

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,3 +1,8 @@
 Rails.configuration.que.tap do |que|
+  # The
+  #   ... || "off")
+  # is required here as YAML converts the string "off"
+  # into the primitive value `false` and blows up
+  # calling `to_sym` :(
   que.mode = (Settings.que.mode || "off").to_sym
 end
