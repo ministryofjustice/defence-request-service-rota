0711e6e47576cefd06c9dd202e24c87882cae7aa
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-08 09:33:40 +0100

    Firm up BankHolidayHelpers test helper

--- spec/support/bank_holiday_helpers.rb
+++ spec/support/bank_holiday_helpers.rb
@@ -1,6 +1,7 @@
 module BankHolidayHelpers
   def stub_bank_holidays!(contents)
     allow(File).to receive(:exists?).with(Settings.bank_holidays.file_location).and_return(true)
+    allow(File).to receive(:open).with(anything).and_call_original
     allow(File).to receive(:open).with(Settings.bank_holidays.file_location).and_return(contents)
   end
 end
